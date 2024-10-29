67d43fcb1a37c69de7fcb076a441b99d8cb2caac
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-10-29 09:07:38 -0600

    convertToDateString (YYYY-MM-DD) helper created

--- app/(helpers)/date.ts
+++ app/(helpers)/date.ts
@@ -1,1 +1,10 @@
 export const getTodaysDate = () => new Date().toISOString().split("T")[0];
+
+// YYYY-MM-DD
+export const convertToDateString = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+};
