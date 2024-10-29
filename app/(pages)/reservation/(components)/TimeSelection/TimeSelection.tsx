b91c6accc7338505730acc5beda5a55282f8f727
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-10-29 07:57:24 -0600

    availableHoursList dynamic rendering logic added in TimeSelection component

--- app/(pages)/reservation/(components)/TimeSelection/TimeSelection.tsx
+++ app/(pages)/reservation/(components)/TimeSelection/TimeSelection.tsx
@@ -1,24 +1,21 @@
 import { Card } from "flowbite-react";
 import React from "react";
 
-const TimeSelection = () => {
+interface TimeSelectionProps {
+  availableHoursList: string[];
+}
+
+const TimeSelection = ({ availableHoursList }: TimeSelectionProps) => {
   return (
     <>
-      <Card className="max-w-sm">
-        <p className="text-center font-normal text-gray-700 dark:text-gray-400">
-          10:00
-        </p>
-      </Card>
-      <Card className="max-w-sm">
-        <p className="text-center font-normal text-gray-700 dark:text-gray-400">
-          11:00
-        </p>
-      </Card>
-      <Card className="max-w-sm">
-        <p className="text-center font-normal text-gray-700 dark:text-gray-400">
-          12:00
-        </p>
-      </Card>
+      {availableHoursList.length > 0 &&
+        availableHoursList.map((hour) => (
+          <Card key={hour} className="max-w-sm">
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+              {hour}
+            </p>
+          </Card>
+        ))}
     </>
   );
 };
