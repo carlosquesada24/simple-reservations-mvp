b5f27d93fb282f975a3ca0b0b9cca5628783e918
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-10-29 07:56:00 -0600

    export word added

--- app/(data)/(reservations)/index.ts
+++ app/(data)/(reservations)/index.ts
@@ -1,4 +1,4 @@
-const availabilityByReservationDate = {
+export const availabilityByReservationDate = {
     '2024-10-29': ['09:00'],
     '2024-10-30': ['09:00', '10:00', '11:00', '12:00'],
     '2024-10-31': ['10:00', '11:00', '13:00'],
