4fadda38307be60a939c49ec17266c5d316e6fc5
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-10-29 09:16:51 -0600

    AvailabilityByReservationDate interface created to avoid errors

--- app/(data)/(reservations)/index.ts
+++ app/(data)/(reservations)/index.ts
@@ -1,4 +1,8 @@
-export const availabilityByReservationDate = {
+interface AvailabilityByReservationDate {
+    [key: string]: string[];
+}
+
+export const availabilityByReservationDate: AvailabilityByReservationDate = {
     '2024-10-29': ['09:00'],
     '2024-10-30': ['09:00', '10:00', '11:00', '12:00'],
     '2024-10-31': ['10:00', '11:00', '13:00'],
