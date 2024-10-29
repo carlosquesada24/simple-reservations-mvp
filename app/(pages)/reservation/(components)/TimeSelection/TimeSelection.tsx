6b920587773c030d810df19e7d70b8baee2182ec
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-10-29 13:31:22 -0600

    TimeCard bg and text changed when is selected

--- app/(pages)/reservation/(components)/TimeSelection/TimeSelection.tsx
+++ app/(pages)/reservation/(components)/TimeSelection/TimeSelection.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 interface TimeSelectionProps {
   availableHoursList: string[];
+  selectedTime: string;
   handleSelectTime: Function;
 }
 
 const TimeSelection = ({
   availableHoursList,
+  selectedTime,
   handleSelectTime,
 }: TimeSelectionProps) => {
   const isAvailableHoursListValid =
@@ -16,17 +18,23 @@ const TimeSelection = ({
   return (
     <>
       {isAvailableHoursListValid &&
-        availableHoursList.map((hour) => (
-          <Card
-            key={hour}
-            className="max-w-sm"
-            onClick={() => handleSelectTime("reservationTime", hour)}
-          >
-            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
-              {hour}
-            </p>
-          </Card>
-        ))}
+        availableHoursList.map((hour) => {
+          const isSelected = selectedTime === hour;
+
+          return (
+            <Card
+              key={hour}
+              className={`max-w-sm ${isSelected && "bg-blue-700"}`}
+              onClick={() => handleSelectTime("reservationTime", hour)}
+            >
+              <p
+                className={`text-center font-normal text-gray-700 dark:text-gray-400 ${isSelected && "text-white"}`}
+              >
+                {hour}
+              </p>
+            </Card>
+          );
+        })}
     </>
   );
 };
