388a2d3bc029804b19428679b88afc31792ec660
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-13 11:13:47 -0600

    Unused code deleted

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Avatar, DarkThemeToggle } from "flowbite-react";
+import React from "react";
+import { DarkThemeToggle } from "flowbite-react";
 import ReservationsList from "./(modules)/(reservations)/(reservation-display)/(show-all-reservations)/(components)/ReservationsList/ReservationsList";
 import { reservationsList } from "./(data)/(reservations)";
 
 export default function Home() {
-  const isBarberUser = true;
-
   return (
     <main className="">
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
