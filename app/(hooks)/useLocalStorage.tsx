f39ee41f1b7dc7323df809fc0a27a1bfb090a7cd
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-13 14:43:41 -0600

    useLocalStorage added

---
+++ app/(hooks)/useLocalStorage.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useState, useEffect } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value, initialized with a generic type T
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored JSON or return initialValue if not found
+      return item ? (JSON.parse(item) as T) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have the same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return { storedValue, setValue };
+}
+
+export default useLocalStorage;
