6af755a5daf0239a407705a2729837795645fde9
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-13 09:54:06 -0600

    ReservationList render reservations dinamically

--- app/(modules)/(show-all-reservations)/(components)/ReservationsList/ReservationsList.tsx
+++ app/(modules)/(show-all-reservations)/(components)/ReservationsList/ReservationsList.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import ReservationCard from "@/app/(components)/ReservationCard/ReservationCard";
+import { Reservation } from "@/app/(data)/(reservations)";
+import { Button } from "flowbite-react";
 
-const ReservationsList = () => {
+interface ReservationsListProps {
+  reservationsList: Reservation[];
+}
+
+const ReservationsList = ({ reservationsList = [] }: ReservationsListProps) => {
+  const isValidArray = reservationsList && Array.isArray(reservationsList);
+  const isEmptyArray = isValidArray && reservationsList.length === 0;
+
   return (
     <div className="flow-root">
-      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-        <li className="py-3 sm:py-4">
-          <ReservationCard
-            salePrice="CRC 3,500.00"
-            durationInMinutes={45}
-            reservationDate="Viernes 15 de noviembre"
-            reservationTime="10:00 a.m."
-          />
-        </li>
-        <li className="py-3 sm:py-4">
-          <ReservationCard
-            salePrice="CRC 3,500.00"
-            durationInMinutes={45}
-            reservationDate="Lunes 14 de octubre"
-            reservationTime="02:00 p.m."
-          />
-        </li>
-      </ul>
+      {isEmptyArray ? (
+        <div className="justify-content-center flex flex-col items-center">
+          <p className="text-center">¡No hay reservas!</p>
+          <Button>Reservar</Button>
+        </div>
+      ) : (
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {reservationsList.map((reservation) => (
+            <li key={reservation.id} className="py-3 sm:py-4">
+              <ReservationCard
+                salePrice={reservation.salePrice}
+                durationInMinutes={reservation.durationInMinutes}
+                reservationDate={reservation.reservationDate}
+                reservationTime={reservation.reservationTime}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
