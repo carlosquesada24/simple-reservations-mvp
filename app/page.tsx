7999ceb5c4701af215bfa7cc538fe04233d3a753
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-13 14:39:06 -0600

    fetch reservations successfully added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+
+import { useEffect } from "react";
 import { DarkThemeToggle } from "flowbite-react";
 import ReservationsList from "./(modules)/(show-all-reservations)/(components)/ReservationsList/ReservationsList";
 import { reservationsList } from "./(data)/(reservations)";
+import supabase from "./(utils)/supabase";
 
 export default function Home() {
+  useEffect(() => {
+    const getAllReservations = async (): Promise<any[]> => {
+      const { data, error } = await supabase.from("Users").select(`
+        id,
+        created_at,
+        UsersTypes (name),
+        name
+        `);
+
+      if (error) {
+        console.log({ error });
+      }
+
+      return data as any[];
+    };
+
+    const handleFetchAllReservations = async () => {
+      const fetchedReservationsList = await getAllReservations();
+      console.log({ fetchedReservationsList });
+    };
+
+    handleFetchAllReservations();
+  }, []);
+
   return (
     <main className="">
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
