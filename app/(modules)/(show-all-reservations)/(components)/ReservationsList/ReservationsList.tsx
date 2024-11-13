31b334633f577516202f16fa05c7c3385f2c21cd
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-13 09:42:26 -0600

    ReservationCards hardcoded in ReservationsList

--- app/(modules)/(show-all-reservations)/(components)/ReservationsList/ReservationsList.tsx
+++ app/(modules)/(show-all-reservations)/(components)/ReservationsList/ReservationsList.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ReservationCard from "@/app/(components)/ReservationCard/ReservationCard";
 
 const ReservationsList = () => {
-  return <div>ReservationsList</div>;
+  return (
+    <div className="flow-root">
+      <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <li className="py-3 sm:py-4">
+          <ReservationCard
+            salePrice="CRC 3,500.00"
+            durationInMinutes={45}
+            reservationDate="Viernes 15 de noviembre"
+            reservationTime="10:00 a.m."
+          />
+        </li>
+        <li className="py-3 sm:py-4">
+          <ReservationCard
+            salePrice="CRC 3,500.00"
+            durationInMinutes={45}
+            reservationDate="Lunes 14 de octubre"
+            reservationTime="02:00 p.m."
+          />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ReservationsList;
