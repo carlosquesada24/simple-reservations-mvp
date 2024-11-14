3294bdfe020000713c8b8e256fb1eae2c9d08cd9
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-14 10:23:24 -0600

    Simple error handling if user doesnt save successfully

--- app/(repositories)/usersRepository.ts
+++ app/(repositories)/usersRepository.ts
@@ -6,11 +6,19 @@ const getUserById = async <T>(clientId: string): Promise<boolean> => {
     const resultUserExists = await supabase.from("Users").select("id").eq("id", clientId);
     const userExists = (resultUserExists !== null && resultUserExists?.data) && resultUserExists?.data[0]?.id.length > 0;
 
+    if (resultUserExists.error) {
+        console.log(resultUserExists.error)
+    }
+
     return userExists ?? false
 };
 
 const saveUser = async <T>(user: T): Promise<SupabaseRecord<T>> => {
     const resultUserCreated = await supabase.from("Users").insert(user).select();
+
+    if (resultUserCreated.error) {
+        console.log(resultUserCreated.error)
+    }
 
     const userCreated = (resultUserCreated !== null && resultUserCreated.data !== null) ? resultUserCreated.data[0] : {};
 
