78a8bc477ad41ba372a7b9153f75b95991cc637c
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-11-14 10:03:36 -0600

    usersRepository created

---
+++ app/(repositories)/usersRepository.ts
@@ -0,0 +1,24 @@
+import supabase from "@/app/(utils)/supabase";
+
+type SupabaseRecord<T> = T & { id: string };
+
+const getUserById = async <T>(clientId: string): Promise<boolean> => {
+    const resultUserExists = await supabase.from("Users").select("id").eq("id", clientId);
+    const userExists = (resultUserExists !== null && resultUserExists?.data) && resultUserExists?.data[0]?.id.length > 0;
+
+    return userExists ?? false
+};
+
+const saveUser = async <T>(user: T): Promise<SupabaseRecord<T>> => {
+    const resultUserCreated = await supabase.from("Users").insert(user).select();
+
+    const userCreated = (resultUserCreated !== null && resultUserCreated.data !== null) ? resultUserCreated.data[0] : {};
+
+    return userCreated as SupabaseRecord<T>;
+};
+
+
+export default {
+    getUserById,
+    saveUser,
+};
